7c143d024cbfde6375b1efe2fbc874761f37ecd5
Kristoffer Berglund <kristofferberglund90@gmail.com>
2023-12-02 14:54:28 +0100

    Day 2

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -1,13 +1,88 @@
-const first = (input: string) => {
-  return 'solution 1';
+type Key = {
+  [key: string]: number;
 };
 
-const expectedFirstSolution = 'solution 1';
+type Cube = {
+  color: string;
+  number: number;
+};
+
+type Game = {
+  id: number;
+  sets: Cube[][];
+};
+
+const bag: Key = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+const play = (games: Game[]): number => {
+  return games
+    .map((game) => {
+      for (let set of game.sets) {
+        for (let cube of set) {
+          if (cube.number > bag[cube.color]) return 0;
+        }
+      }
+      return game.id;
+    })
+    .reduce((acc, curr) => acc + curr);
+};
+
+const calc = (games: Game[]): number => {
+  return games
+    .map((game) => {
+      const power: Key = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      for (let set of game.sets) {
+        for (let cube of set) {
+          if (cube.number > power[cube.color]) {
+            power[cube.color] = cube.number;
+          }
+        }
+      }
+      return power.red * power.green * power.blue;
+    })
+    .reduce((acc, curr) => acc + curr);
+};
+
+const load = (input: string): Game[] => {
+  return input.split('\n').map((line) => {
+    const part = line.split(':');
+    const gameId = part[0].split(' ')[1];
+    const gameSets = part[1].split(';').map((value) => {
+      let set: Cube[] = [];
+      let cubes = value.split(',');
+      for (let cube of cubes) {
+        let val = cube.split(' ');
+        set.push({ number: Number(val[1]), color: val[2] });
+      }
+      return set;
+    });
+
+    return { id: Number(gameId), sets: gameSets };
+  });
+};
+
+const first = (input: string) => {
+  const games: Game[] = load(input);
+  const result = play(games);
+  return result.toString();
+};
+
+const expectedFirstSolution = '3035';
 
 const second = (input: string) => {
-  return 'solution 2';
+  const games: Game[] = load(input);
+  const power = calc(games);
+  return power.toString();
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = '66027';
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
